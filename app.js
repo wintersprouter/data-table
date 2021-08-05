d3cd94339a70c40530f6d337952fb2fdb355bce4
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-05 20:01:24 +0800

    feat: render table rows

--- app.js
+++ app.js
@@ -22,3 +22,73 @@ const darkModeToggleHandler = (event) => {
 }
 // bind the event
 darkModeToggle.addEventListener('change', darkModeToggleHandler)
+
+// render table row
+const data = 25
+const dataPanel = document.querySelector('.table__body')
+function renderTableRows (data) {
+  let rawHTML = ' '
+
+  for (let i = 1; i < data; i++) {
+    rawHTML += `
+      <tr class="table__row">
+        <td class="table__cell table__cell--checkbox">
+          <input type="checkbox" />
+        </td>
+          <td class="table__cell table__cell--id">${i}</td>
+          <td class="table__cell table__cell--name">NewYear${i}</td>
+          <td class="table__cell table__cell--advertiser">
+            <span class="cell__advertiser__line">Food Market</span>
+            <span class="cell__advertiser__line cell__advertiser__line--group">
+              Kelly
+            </span>
+          </td>
+          <td class="table__cell table__cell--description">holiday night during chr...</td>
+          <td class="table__cell table__cell--price">$2000</td>
+          <td class="table__cell table__cell--starttime">2020/12/24 ${i}:30</td>
+          <td class="table__cell table__cell--endtime">2021/01/02 ${i}:00</td>
+          <td class="table__cell table__cell--action">
+          <img
+            class="cell__action__icon"
+            src="https://raw.githubusercontent.com/WendellLiu/alphacamp-frontend-course/master/data-table/static/icons/menu.svg"
+            id="action__input_${i}"
+            alt="menu"
+          />
+          <div
+            class="action__menu hidden"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="action__input_${i}"
+            id="action__menu_${i}"
+          >
+          <menu class="menu__body">
+            <menuitem class="menu__item">
+              <img
+                src="https://raw.githubusercontent.com/WendellLiu/alphacamp-frontend-course/master/data-table/static/icons/duplicate.svg"
+                class="menu__item__icon"
+              />
+              <span>Duplicate</span>
+            </menuitem>
+            <menuitem class="menu__item">
+              <img
+                src="https://raw.githubusercontent.com/WendellLiu/alphacamp-frontend-course/master/data-table/static/icons/edit.svg"
+                class="menu__item__icon"
+              />
+              <span>Edit</span>
+            </menuitem>
+            <menuitem class="menu__item">
+              <img
+                src="https://raw.githubusercontent.com/WendellLiu/alphacamp-frontend-course/master/data-table/static/icons/delete.svg"
+                class="menu__item__icon"
+              />
+              <span>Delete</span>
+            </menuitem>
+          </menu>
+        </div>
+      </td>
+    </tr>
+    `
+  }
+  dataPanel.innerHTML = rawHTML
+}
+renderTableRows(data)
