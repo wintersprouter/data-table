87821e03231622f3f46f60fedda77f1c2054cbca
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-05 16:25:39 +0800

    style: add dark mode layout

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ const toggleMenu = (event) => {
 }
 // bind the event
 input.addEventListener('click', toggleMenu)
+
+// dark mode
+// target the switch element
+const darkModeToggle = document.getElementById('dark__mode__toggle')
+// toggle handler
+const darkModeToggleHandler = (event) => {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
+}
+// bind the event
+darkModeToggle.addEventListener('change', darkModeToggleHandler)
